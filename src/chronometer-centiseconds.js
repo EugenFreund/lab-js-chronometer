9df87949eb063bca7b2c5d384593053963cc09f3
Eugen Freund <Eugen.Freund@outlook.de>
2023-09-13 09:08:06 +0200

    Add chronometer and chronometer-centiseconds class

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,55 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval( () => {
+      this.currentTime += 1;
+      if(typeof(printTimeCallback) === "function" ) {
+        printTimeCallback();
+      }
+    }, 10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime % 360000) / 6000)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime % 6000) / 100);
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return this.currentTime % 100 ;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let retVal = "";
+    value = value.toString()
+    let splitDigitArray = value.split('');
+    if( splitDigitArray.length < 2){
+      splitDigitArray.unshift("0")
+    } 
+    retVal = splitDigitArray.join("");
+
+    return retVal;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}.${this.computeTwoDigitNumber(this.getCentiseconds())}`;
   }
 }
+
+let test = new Chronometer();
+test.currentTime = 5021000;
+console.log(test.getMinutes())
